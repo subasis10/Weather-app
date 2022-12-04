2611f6aeaa4547a956d41bbdb403cde754cac3d6
Shubhashish Basnet <saaramsbasnet@gmail.com>
2022-12-04 08:14:19 +0545

    weather app

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const path = require('path');
+const hbs = require('hbs');
+const app = express();
+// const partials = require('partials');
+const port = process.env.PORT || 3000;
+
+// public static path joining src and public file
+const static_path = path.join(__dirname, "../public");
+const template_path = path.join(__dirname, "../templates/views");
+const partial_path = path.join(__dirname, "../templates/partials");
+
+app.set('view engine', 'hbs');
+app.set('views', template_path);
+hbs.registerPartials(partial_path);
+
+app.use(express.static(static_path));
+
+
+// routing methods
+app.get("/", (req, res) => {
+    res.render("index");
+})
+
+app.get("/about", (req, res) => {
+    res.render("about");
+})
+
+app.get("/weather", (req, res) => {
+    res.render("weather");
+})
+
+app.get("*", (req, res) => {
+    res.render("404error");
+})
+
+app.listen(port, function () {
+    console.log(`listening to port at ${port}`);
+})
